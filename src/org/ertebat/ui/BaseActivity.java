bc2ecd385a326eddc81283d1e6f45f6d64496d6b
Morteza Sabetraftar <morteza.sabetraftar@gmail.com>
2014-12-14 01:08:18 +0330

    + Moved NGN initialization from MainActivity to BaseActivity

--- src/org/ertebat/ui/BaseActivity.java
+++ src/org/ertebat/ui/BaseActivity.java
@@ -10,11 +10,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.doubango.ngn.NgnEngine;
+import org.doubango.ngn.events.NgnEventArgs;
+import org.doubango.ngn.events.NgnInviteEventArgs;
 import org.doubango.ngn.events.NgnInviteEventTypes;
+import org.doubango.ngn.events.NgnMessagingEventArgs;
+import org.doubango.ngn.events.NgnMsrpEventArgs;
+import org.doubango.ngn.events.NgnRegistrationEventArgs;
+import org.doubango.ngn.media.NgnMediaType;
+import org.doubango.ngn.model.NgnHistorySMSEvent;
+import org.doubango.ngn.model.NgnHistoryEvent.StatusType;
 import org.doubango.ngn.services.INgnConfigurationService;
 import org.doubango.ngn.services.INgnHistoryService;
 import org.doubango.ngn.services.INgnSipService;
 import org.doubango.ngn.sip.NgnAVSession;
+import org.doubango.ngn.sip.NgnMsrpSession;
+import org.doubango.ngn.utils.NgnDateTimeUtils;
+import org.doubango.ngn.utils.NgnStringUtils;
+import org.doubango.ngn.utils.NgnUriUtils;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -23,6 +35,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
@@ -55,7 +68,7 @@ public class BaseActivity extends FragmentActivity {
 	protected Handler mHandler;
 	protected FragmentDialogResultListener mFragmentDialogListener;
 	protected String TAG = "BaseActivity";
-	protected String RestServer = "http://localhost:4000/";
+	protected String RestServer = "http://localhost:4000/";	
 
 	//Websocket service
 	protected Intent mWebsocketIntent;
@@ -173,6 +186,7 @@ public class BaseActivity extends FragmentActivity {
 			FontKoodak = Typeface.createFromAsset(getAssets(), "bkoodkbd.ttf");
 		if (FontNazanin == null)
 			FontNazanin = Typeface.createFromAsset(getAssets(), "bnazaninbd_0.ttf");
+
 		try{
 			mWebsocketIntent = new Intent(IWebsocketService.class.getName());
 			startService(mWebsocketIntent);
@@ -180,6 +194,178 @@ public class BaseActivity extends FragmentActivity {
 		}
 		catch(Exception ex){
 			Log.d(TAG, ex.getMessage());
+		}
+		
+		try
+		{
+			mEngine = NgnEngine.getInstance();
+			mConfigurationService = mEngine.getConfigurationService();
+			mSipService = mEngine.getSipService();
+			mSipBroadCastRecv = new BroadcastReceiver() {
+				@Override
+				public void onReceive(Context context, Intent intent) {
+					final String action = intent.getAction();
+
+					// Registration Event
+					if(NgnRegistrationEventArgs.ACTION_REGISTRATION_EVENT.equals(action)){
+						NgnRegistrationEventArgs args = intent.getParcelableExtra(NgnEventArgs.EXTRA_EMBEDDED);
+						if(args == null){
+							return;
+						}
+						switch(args.getEventType()){
+						case REGISTRATION_NOK:
+							showToast("Failed to register :(");
+							break;
+						case UNREGISTRATION_OK:
+							showToast("You are now unregistered :)");
+							break;
+						case REGISTRATION_OK:
+							showToast("You are now registered :)");
+							break;
+						case REGISTRATION_INPROGRESS:
+							showToast("Trying to register...");
+							break;
+						case UNREGISTRATION_INPROGRESS:
+							showToast("Trying to unregister...");
+							break;
+						case UNREGISTRATION_NOK:
+							showToast("Failed to unregister :(");
+							break;
+						}
+					}
+					//Begin Of SIP Invite Segment
+					else if(NgnInviteEventArgs.ACTION_INVITE_EVENT.equals(action)){
+						NgnInviteEventArgs args = intent.getParcelableExtra(NgnEventArgs.EXTRA_EMBEDDED);
+						if(args == null){
+							return;
+						}
+
+						final NgnMediaType mediaType = args.getMediaType();
+						mAVSession = NgnAVSession.getSession(args.getSessionId());
+						if(NgnMediaType.isAudioVideoType(mediaType))
+							SetCallState(args.getEventType());
+						switch(args.getEventType()){							
+						case TERMWAIT:
+						case TERMINATED:
+							if(NgnMediaType.isAudioVideoType(mediaType)){
+								//Show UI Function that indicate to terminated call
+								//NgnInviteEventTypes
+								mEngine.getSoundService().stopRingBackTone();
+								mEngine.getSoundService().stopRingTone();
+							}
+							break;
+
+						case INCOMING:
+							if(NgnMediaType.isAudioVideoType(mediaType)){
+								if(mAVSession != null){			
+									mEngine.getSoundService().startRingTone();
+
+									// CHECK
+//									Intent incomingIntent = new Intent(This, CallActivity.class);
+//									incomingIntent.putExtra(NgnEventArgs.EXTRA_EMBEDDED, args);
+//									startActivity(incomingIntent);
+//									finish();
+								}
+								else{
+								}
+							}
+							break;
+
+						case INPROGRESS:
+							if(NgnMediaType.isAudioVideoType(mediaType)){
+								//mEngine.showAVCallNotif(R.drawable.phone_call_25, getString(R.string.string_call_outgoing));
+								//Trying to send invite to remote party
+							}
+							break;
+
+						case RINGING:
+							if(NgnMediaType.isAudioVideoType(mediaType)){
+								mEngine.getSoundService().startRingBackTone();
+							}
+							break;
+
+						case CONNECTED:
+						case EARLY_MEDIA:
+							if(NgnMediaType.isAudioVideoType(mediaType)){
+								mEngine.getSoundService().stopRingBackTone();
+								mEngine.getSoundService().stopRingTone();
+							}
+							break;
+
+						case MEDIA_UPDATED:
+							showToast("Media Updated");
+							break;
+						default: break;
+						}
+					}
+					//End Of Invite Segment
+					//Chat Section 
+					else if(NgnMessagingEventArgs.ACTION_MESSAGING_EVENT.equals(action)){
+						NgnMessagingEventArgs args = intent.getParcelableExtra(NgnMessagingEventArgs.EXTRA_EMBEDDED);
+						if(args == null){
+							return;
+						}
+						switch(args.getEventType()){
+						case INCOMING:
+							String dateString = intent.getStringExtra(NgnMessagingEventArgs.EXTRA_DATE);
+							String remoteParty = intent.getStringExtra(NgnMessagingEventArgs.EXTRA_REMOTE_PARTY);
+							if(NgnStringUtils.isNullOrEmpty(remoteParty)){
+								remoteParty = NgnStringUtils.nullValue();
+							}
+							remoteParty = NgnUriUtils.getUserName(remoteParty);
+							NgnHistorySMSEvent event = new NgnHistorySMSEvent(remoteParty, StatusType.Incoming);
+							event.setContent(new String(args.getPayload()));
+							event.setStartTime(NgnDateTimeUtils.parseDate(dateString).getTime());
+							mEngine.getHistoryService().addEvent(event);
+							//ShowIncomingMessage(remoteParty, dateString, event.getContent());
+							//ReloadMessageList();
+							break;
+						default:
+							break;
+						}
+					}
+
+					// MSRP chat Events
+					// For performance reasons, file transfer events will be handled by the owner of the context
+					else if(NgnMsrpEventArgs.ACTION_MSRP_EVENT.equals(action)){
+						NgnMsrpEventArgs args = intent.getParcelableExtra(NgnMsrpEventArgs.EXTRA_EMBEDDED);
+						if(args == null){
+							return;
+						}
+						switch(args.getEventType()){
+						case DATA:
+							final NgnMsrpSession session = NgnMsrpSession.getSession(args.getSessionId());
+							if(session == null){
+								return;
+							}
+							final byte[]content = intent.getByteArrayExtra(NgnMsrpEventArgs.EXTRA_DATA);
+							NgnHistorySMSEvent event = new NgnHistorySMSEvent(NgnUriUtils.getUserName(session.getRemotePartyUri()), StatusType.Incoming);
+							event.setContent(content==null ? NgnStringUtils.nullValue() : new String(content));
+							mEngine.getHistoryService().addEvent(event);
+							//ShowIncomingMessage("Pager", "Now", event.getContent());
+							//ReloadMessageList();
+							break;
+						default:
+							break;
+						}
+					}
+				}
+
+			};
+			final IntentFilter intentFilter = new IntentFilter();
+			intentFilter.addAction(NgnRegistrationEventArgs.ACTION_REGISTRATION_EVENT);
+			intentFilter.addAction(NgnInviteEventArgs.ACTION_INVITE_EVENT);
+			intentFilter.addAction(NgnMsrpEventArgs.ACTION_MSRP_EVENT);
+			intentFilter.addAction(NgnMessagingEventArgs.ACTION_MESSAGING_EVENT);
+			registerReceiver(mSipBroadCastRecv, intentFilter);
+			
+			if(mEngine.isStarted() == false)
+				mEngine.start();
+		}
+		catch(Exception ex)
+		{
+			Log.d("BASE", ex.getMessage());
+			showToast(ex.getMessage());
 		}
 	}
 
